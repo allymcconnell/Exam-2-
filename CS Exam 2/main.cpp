cdeece82d9eed2abd714bba587273332289396df
allymcconnell <amcconnell16@my.whitworth.edu>
2016-06-21 08:46:05 -0700

    first commit

--- CS Exam 2/main.cpp
+++ CS Exam 2/main.cpp
@@ -1,15 +1,56 @@
 //
 //  main.cpp
 //  CS Exam 2
-//
+// I affirm that all code given below was written solely by me, Ally McConnell, and that any help I received adhered to the rules stated for this exam.
 //  Created by Ally McConnell on 6/21/16.
 //  Copyright © 2016 Ally McConnell. All rights reserved.
 //
 
 #include <iostream>
+using namespace std;
+#include <string>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Runner
+{
+private:
+    string firstname;
+    string lastname;
+    int pace;
+    
+public:
+    string get_firstname();
+    string get_lastname();
+    int get_pace();
+};
+
+Runner::get_firstname()
+{
+    return firstname;
+}
+Runner::get_lastname();
+{
+    return lastname;
+}
+Runner::get_pace();
+{
+    return pace;
+}
+
+int main()
+{
+    string line;
+    ifstream myfile ("registrants.txt");
+    if (myfile.is_open())
+    {
+        while (getLine (myfile, line))
+        {
+            cout << line << '\n';
+        }
+        myfile.close()
+    }
+    else cout << "Unable to open file";
     return 0;
 }
+
+
