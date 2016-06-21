35934407022ecc613c36e1e1abe83d8667179807
allymcconnell <amcconnell16@my.whitworth.edu>
2016-06-21 07:05:25 -0700

    Initial Commit

---
+++ CS Exam 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS Exam 2
+//
+//  Created by Ally McConnell on 6/21/16.
+//  Copyright © 2016 Ally McConnell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
